823ea88a322446087a688bc876c6cde9384f298e
Daniel <dandcalvo@gmail.com>
2024-07-09 10:40:29 -0700

    merge sort and remove duplicates

--- index.js
+++ index.js
@@ -14,27 +14,54 @@ class Tree {
     root;
 
     constructor(array) {
-        root = buildTree;
+        this.root = buildTree(cleanArray(array));
     }
 
-    buildTree() {}
-}
-
-function mergeSort(array) {
-    if (array.length === 1) {
-        return array;
+    buildTree(array) {
+        
     }
 
-    const midpoint = Math.floor(array.length / 2);
-    const left = mergeSort(array.slice(0, midpoint));
-    const right = mergeSort(array.slice(midpoint));
-    const result = [];
+    cleanArray(array) {
+        return removeDuplicates(mergeSort(array));
+    }
 
-    while (left && right) {
-        if (left[0] <= right[0]) {
-            
+    mergeSort(array) {
+        if (array.length === 1) {
+            return array;
         }
+
+        const midpoint = Math.floor(array.length / 2);
+        const left = mergeSort(array.slice(0, midpoint));
+        const right = mergeSort(array.slice(midpoint));
+        let result = [];
+
+        while (left.length && right.length) {
+            if (left[0] <= right[0]) {
+                result.push(left.shift());
+            } else {
+                result.push(right.shift());
+            }
+        }
+
+        if (left.length) {
+            result = result.concat(left);
+        }
+
+        if (right.length) {
+            result = result.concat(right);
+        }
+
+        return result;
+    }
+
+    removeDuplicates(array) {
+        const set = new Set(array);
+        const result = [];
+
+        for (const entry of set) {
+            result.push(entry);
+        }
+
+        return result;
     }
 }
-
-console.log(mergeSort([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
